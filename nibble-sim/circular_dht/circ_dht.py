48a6d599271c5ad3a189b1392e9df0f91a92817b
meetm473 <meetmatehere@gmail.com>
2021-03-24 10:17:16 +0530

    Added files defining the basic simulator structure

---
+++ nibble-sim/circular_dht/circ_dht.py
@@ -0,0 +1,13 @@
+"""
+    Implementation of circular distributed hash table.
+"""
+
+import sys
+import os
+sys.path.append(os.path.dirname(__file__)+'/..')
+
+from common.node import Node
+
+class CircularDht(Node):
+    pass
+
---
+++ nibble-sim/nibble_sim.py
@@ -0,0 +1,9 @@
+"""
+    Main script executing nibble-sim
+"""
+
+if __name__=='__main__':
+    print('Welcome to nibble-sim!')
+    print('----------------------')
+    print('Developed by Mayank Mehta and Vishva Bhate\n')
+    pass
