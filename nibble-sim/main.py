31360b3fb0ed322b5db0cf8fab2701233cc9fcfc
vish897 <vishvabhate@gmail.com>
2021-04-04 11:51:32 +0530

    Chatbox done

---
+++ nibble-sim/main.py
@@ -0,0 +1,36 @@
+'''
+Main file (To be run in each terminal)
+'''
+
+# Importing nodeObject class
+import nodeConnection
+import sys
+
+if __name__ == '__main__':
+    
+
+
+    host = input("Enter host for the node: ")
+    portNo = input("Enter port number for the node: ")
+
+    node = nodeConnection.nodeObject(host, int(portNo))
+    
+    print("Please enter the message to be sent to the node in the following format")
+    print("send(msg, host, port number)")
+    print("-----------------------------------------------------------------------")
+    print("Chatbox begins:")
+    print("_______________________________________________________________________")
+    
+    while True:
+        try:
+            
+            cmd = input()
+            if cmd == 'send':
+                msg = input("Enter the message: ")
+                host = input("Enter the target host: ")
+                portNo = input("Enter the target port number: ")
+                node.ConnAndSend(msg, host, int(portNo))
+
+        except KeyboardInterrupt:
+            sys.exit()
+            break
